bf280d84450757116b0aa87d9b61a2c45d2f586a
heerakmoon <heerakmoon@gmail.com>
2025-03-26 21:57:43 -0700

    Function to get a random item from an array

---
+++ src/utils/getRandomFromArr.js
@@ -0,0 +1,7 @@
+const getRandomFromArr = (arr) => {
+    const randomIndex = Math.floor(Math.random * arr.length);
+    const randomItem = arr[randomIndex];
+    return randomItem;
+}
+
+export default getRandomFromArr;
