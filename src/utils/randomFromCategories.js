54ecb837f7945d18a6ac6c217cde6eab168985a4
heerakmoon <heerakmoon@gmail.com>
2025-03-27 18:26:35 -0700

    Function to fetch full recipe after selecting a random meal from a category

--- src/utils/randomFromCategories.js
+++ src/utils/randomFromCategories.js
@@ -1,24 +1,28 @@
-// pick a random category from the list of category picked by user
-// use endpoint that gets a random recipe from a category
-const randomFromCategories = (categoriesArr) => {
-    const a = Math.floor(Math.random() * categoriesArr.length);
-    const randomCategory = categoriesArr[a];
-    const recipesArr = getRecipesFromCategory();
+import getRandomFromArr from "./getRandomFromArr";
+import transformRecipeData from "./transformRecipeData";
 
-    const getRecipesFromCategory = async () => {
-        try {
-            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${randomCategory}`);
-            if (!res.ok) {
-                throw new Error(`Failed to get recipes from ${randomCategory} category`);
-            }
-            const data = res.json();
-            return data;
-        } catch (err) {
-            console.error(err);
+const randomFromCategories = async (categoriesArr) => {
+    const randomCategory = getRandomFromArr(categoriesArr);
+    
+    try {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${randomCategory}`); //first api call for all recipes from a category
+        if (!res.ok) {
+            throw new Error(`Failed to get recipes from ${randomCategory} category`);
         }
-    }
+        const data = await res.json();
+        const randomRecipeFromCategory = getRandomFromArr(data.meals);
+        const randomRecipeId = randomRecipeFromCategory.idMeal;
 
-    return 
+        const res2 = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomRecipeId}`); //second api call by recipe id because recipes from first api call doesn't have enough info
+        if (!res2.ok) {
+            throw new Error(`Failed to get recipe #${randomRecipeId}.`);
+        }
+        const data2 = await res2.json();
+        const transformedRecipe = transformRecipeData(data2);
+        return transformedRecipe;
+    } catch (err) {
+        console.error(err.message);
+    }
 }
 
 export default randomFromCategories;
