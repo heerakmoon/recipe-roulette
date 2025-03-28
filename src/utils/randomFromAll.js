6374de8347052a91dddcb194352725a3bacd161f
heerakmoon <heerakmoon@gmail.com>
2025-03-27 18:21:14 -0700

    Function to fetch a random recipe from all meals, moved from Home.jsx to its own module

--- src/utils/randomFromAll.js
+++ src/utils/randomFromAll.js
@@ -1,28 +1,19 @@
-// get a random recipe and pass it to the parent component, RecipeDisplay
-const randomFromAll = async () => {
-    let randomRecipe = null;
-    let error = null;
-    let loading = false;
+// get a random recipe from all recipes
+import transformRecipeData from "./transformRecipeData";
 
-    loading = true;
+const randomFromAll = async () => {
     try {
         const res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
         if (!res.ok) {
             throw new Error('Failed to fetch random recipe');
         }
         const data = await res.json();
-        randomRecipe = data;
+        const transformedRecipe = transformRecipeData(data);
+
+        return transformedRecipe;
     } catch (err) {
-        error = err.message;
-    } finally {
-        loading = false;
+        console.error('Error fetching random recipe:', err.message);
     }
-
-    console.log('randomRecipe, randomFromAll.js: ', randomRecipe);
-    console.log('error from randomFromAll: ', error);
-    console.log('loading from randomFromall: ', loading);
-
-    return randomRecipe;
 }
 
 export default randomFromAll;
