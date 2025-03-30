19685973c9e059b82a02284e785c80da564e7735
heerakmoon <heerakmoon@gmail.com>
2025-03-29 23:33:58 -0700

    when the categories and their labels are hovered, cursor changes to pointer

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 const Category = ({ category }) => {
+  const useHover = () => {
+    const [isHovering, setIsHovering] = useState(false);
+    
+    const  hoverProps = {
+      onMouseEnter: () => setIsHovering(true),
+      onMouseLeave: () => setIsHovering(false),
+      style: {
+        cursor: isHovering ? 'pointer' : 'default'
+      }
+    }
+    return hoverProps;
+  }
+
+  const hoverProps = useHover();
+
   return (
     <div>
-      <input type="checkbox" value={category}/>
-      <label className="pl-1">{category}</label>
+      <input {...hoverProps} type="checkbox" value={category}/>
+      <label {...hoverProps} className="pl-1">{category}</label>
     </div>
   )
 }
