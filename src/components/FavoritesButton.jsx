16397b945643ad2a9f906989aa989f0543425b8b
heerakmoon <heerakmoon@gmail.com>
2025-04-04 09:13:01 -0700

    When users click the favorites button it takes them to /favorites'

--- src/components/FavoritesButton.jsx
+++ src/components/FavoritesButton.jsx
@@ -1,10 +1,13 @@
 import { Star } from "lucide-react";
+import { Link } from "react-router";
 
 const FavoritesButton = () => {
     const classes = localStorage.length ? "text-left cursor-pointer text-[#FFF704]" : "text-left cursor-pointer text-[#858585]";
 
     return (
-        <Star className={classes} />
+        <Link to="/favorites">
+            <Star className={classes} />
+        </Link>
     )
 }
 
