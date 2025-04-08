5137bf6a92d6d5738155a0f91f37b6c7dbfa1f4d
heerakmoon <heerakmoon@gmail.com>
2025-04-08 13:52:40 -0700

    Moved function that gets the recipes from local storage into separate file, incorporated remove recipe functionality so when user clicks the star icon on a recipe card it removes the recipe from the favorites list

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,24 +1,31 @@
 import RecipeCard from "./RecipeCard";
+import NoRecipe from "./NoRecipe";
 import { useState, useEffect } from "react";
+import getRecipesFromLocalStorage from "../utils/getRecipesFromLocalStorage";
 
 const FavoritesList = () => {
     const [favoritesArr, setFavoritesArr] = useState([]);
 
     useEffect(() => {
-        const allFavorites = Object.keys(localStorage).map((key) => {
-            try {
-                return JSON.parse(localStorage.getItem(key));
-            } catch {
-                return null;
-            }
-        })
+        const allFavorites = getRecipesFromLocalStorage();
         setFavoritesArr(allFavorites);
     },  []);
-        
+
+    const removeFromFavorites = (recipeId) => {
+        localStorage.removeItem(recipeId);
+        const updatedFavoritesArr = getRecipesFromLocalStorage();
+        setFavoritesArr(updatedFavoritesArr);
+    }
+
+
     return (
-        <div className="w-3/4 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
-            {favoritesArr.map((recipe) => <RecipeCard recipeInfo={recipe} />)}
-        </div>
+        favoritesArr.length === 0
+            ? <NoRecipe page="favorites" />
+            : (
+                <div className="w-3/4 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
+                    {favoritesArr.map((recipe) => <RecipeCard key={recipe.id} recipeInfo={recipe} onRemove={() => removeFromFavorites(recipe.id)} />)}
+                </div>
+            )
     )
 }
 
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,12 @@
 import { Star } from "lucide-react";
 
-const RecipeCard = ({ recipeInfo }) => {
-    return (
-        <div className="relative border rounded-md border-[#858585]">
-            <Star className="absolute right-4 top-4 cursor-pointer text-[#FFF704] fill-[#FFF704]" />
-            <img className="rounded-t-md" src={recipeInfo.image} alt={recipeInfo.name} />
-            <h6 className="p-4">{recipeInfo.name}</h6>
+const RecipeCard = ({ recipeInfo, onRemove }) => {return (
+        <div className="relative border rounded-lg border-[#858585]">
+            <Star onClick={onRemove} className="absolute right-4 top-4 cursor-pointer text-[#FFF704] fill-[#FFF704]" />
+            <div className="cursor-pointer">
+                <img className="rounded-t-md" src={recipeInfo.image} alt={recipeInfo.name} />
+                <h6 className="p-4">{recipeInfo.name}</h6>
+            </div>
         </div>
     )
 }
