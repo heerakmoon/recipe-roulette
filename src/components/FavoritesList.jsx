888511d52b236b3fb0bb1a035dd0782073f25c5a
heerakmoon <heerakmoon@gmail.com>
2025-04-09 12:23:17 -0700

    Moved modal to component where the favorites list is displayed, passing the data of whether the modal should be shown from RecipeCard component to parent and then to modal component

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,10 +1,20 @@
 import RecipeCard from "./RecipeCard";
 import NoRecipe from "./NoRecipe";
+import RecipeModal from "./RecipeModal";
 import { useState, useEffect } from "react";
 import getRecipesFromLocalStorage from "../utils/getRecipesFromLocalStorage";
 
 const FavoritesList = () => {
     const [favoritesArr, setFavoritesArr] = useState([]);
+    const [viewModal, setViewModal] = useState();
+    const testObj = {
+        favorited: true,
+        id: "52787",
+        image: "https://www.themealdb.com/images/media/meals/xrysxr1483568462.jpg",
+        ingredients: ["2 cups Chocolate Chips", "2 tbs Heavy Cream", "1 14-ounce can Condensed Milk"],
+        instructions: "Line an 8-inch-square baking pan with wax paper or foil, and coat with non-stick spray.\r\nIn a microwave-safe bowl, combine the dark chocolate chips, heavy cream and half of the sweetened condensed milk. Microwave the dark chocolate mixture in 20-second intervals, stirring in between each interval, until the chocolate is melted.\r\nAdd the vanilla extract to the dark chocolate mixture and stir well until smooth.\r\nTransfer the dark chocolate mixture into the prepared pan and spread into an even layer.\r\nIn a separate bowl, combine the white chocolate chips with the remaining half of the sweetened condensed milk. Microwave the white chocolate mixture in 20-second intervals, stirring in between each interval, until the chocolate is melted.\r\nEvenly spread the white chocolate mixture on top of dark chocolate layer.\r\nTop the chocolate layers with the Mallow Bits or miniature marshmallows, and gently press them down.\r\nRefrigerate for 4 hours, or until set.\r\nRemove the fudge and wax paper from the pan. Carefully peel all of the wax paper from the fudge.\r\nCut the fudge into bite-sized pieces and serve.",
+        name: "Hot Chocolate Fudge"
+    }
 
     useEffect(() => {
         const allFavorites = getRecipesFromLocalStorage();
@@ -17,15 +27,23 @@ const FavoritesList = () => {
         setFavoritesArr(updatedFavoritesArr);
     }
 
+    const handleChildData = (data) => {
+        setViewModal(data);
+    }
+
 
     return (
-        favoritesArr.length === 0
-            ? <NoRecipe page="favorites" />
-            : (
-                <div className="w-3/4 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
-                    {favoritesArr.map((recipe) => <RecipeCard key={recipe.id} recipeInfo={recipe} onRemove={() => removeFromFavorites(recipe.id)} />)}
-                </div>
-            )
+        <div>
+            <RecipeModal show={viewModal} recipe={testObj} />
+            <div className="w-3/4 mx-auto grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
+                {
+                    favoritesArr.length === 0
+                        ? <NoRecipe page="favorites" />
+                        : favoritesArr.map((recipe) => <RecipeCard key={recipe.id} recipeInfo={recipe} onRemove={() => removeFromFavorites(recipe.id)} onSendData={handleChildData} />)
+
+                }
+            </div>
+        </div>
     )
 }
 
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,23 +1,19 @@
 import { Star } from "lucide-react";
-import RecipeModal from "./RecipeModal";
-import { useState } from "react";
 
-const RecipeCard = ({ recipeInfo, onRemove }) => {
-    const [viewRecipe, setViewRecipe] = useState(false);
+const RecipeCard = ({ recipeInfo, onRemove, onSendData }) => {
+    let viewRecipe = false;
 
-    const handleClick = () => {
-        setViewRecipe(!viewRecipe);
-        console.log(viewRecipe);
+    const sendView = () => {
+        onSendData(!viewRecipe);
     }
 
     return (
         <div className="relative border rounded-lg border-[#858585]">
             <Star onClick={onRemove} className="absolute right-4 top-4 cursor-pointer text-[#FFF704] fill-[#FFF704]" />
-            <div onClick={handleClick} className="cursor-pointer">
+            <div onClick={sendView} className="cursor-pointer">
                 <img className="rounded-t-md" src={recipeInfo.image} alt={recipeInfo.name} />
                 <h6 className="p-4">{recipeInfo.name}</h6>
             </div>
-            <RecipeModal show={viewRecipe} recipe={recipeInfo} />
         </div>
     )
 }
