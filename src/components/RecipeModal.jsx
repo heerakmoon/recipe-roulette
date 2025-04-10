5da457675063114cd5ab040ec316e3fda671f9f2
heerakmoon <heerakmoon@gmail.com>
2025-04-10 14:36:59 -0700

    Style: Recipe modal appears when a recipe card is clicked to show recipe's ingredients and instructions

--- src/components/RecipeModal.jsx
+++ src/components/RecipeModal.jsx
@@ -3,10 +3,21 @@ const RecipeModal = ({ show, recipe }) => {
   const expandRecipeCard = show ? 'visible' : 'invisible';
   return (
     <div className={expandRecipeCard}>
-      <div>
-        <div className="fixed top-0 left-0 bg-black opacity-50 w-full h-full"></div>
-        <div className="absolute top-1/2 left-1/2 bg-white">
-          <p>{recipe.name}</p>
+      <div className="fixed inset-0 bg-black/50 z-10"></div>
+      <div className="fixed inset-0 flex justify-center items-center z-50">
+        <div className="w-1/2 h-3/4 bg-white rounded-lg overflow-y-auto">
+          <img className="rounded-t-lg w-full h-3/4 object-cover" src={recipe.image} alt={recipe.name} />
+          <div className="p-4">
+            <h5>{recipe.name}</h5>
+            <h5>Ingredients</h5>
+            <ul className="fustat list-disc list-inside p-2">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+            </ul>
+            <h5>Instructions</h5>
+            <p>{recipe.instructions}</p>
+          </div>
         </div>
       </div>
     </div>
