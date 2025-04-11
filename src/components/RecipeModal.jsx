7b07efa61b9c11abbf391c60273ed94bd1a0bba1
heerakmoon <heerakmoon@gmail.com>
2025-04-11 16:21:39 -0700

    Style: added bg-white to X icon for modal component so the icon is easier to see on darker images

--- src/components/RecipeModal.jsx
+++ src/components/RecipeModal.jsx
@@ -7,7 +7,7 @@ const RecipeModal = ({ recipe, modalOff }) => {
     <div onClick={modalOff} className="fixed inset-0 bg-black/50 z-50 cursor-pointer flex justify-center items-center">
       <div onClick={(e) => e.stopPropagation()} className="w-1/2 h-3/4 bg-white rounded-lg overflow-y-auto cursor-default">
         <div className="flex justify-end relative">
-          <X onClick={modalOff} className="absolute p-2 w-[40px] h-[40px] rounded-lg cursor-pointer" />
+          <X onClick={modalOff} className="absolute w-[35px] h-[35px] bg-white rounded-md cursor-pointer" />
           <img className="rounded-t-lg w-full h-3/4 object-cover" src={recipe.image} alt={recipe.name} />
         </div>
         <div className="p-4">
